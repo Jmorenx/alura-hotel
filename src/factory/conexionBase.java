d459a1577a147ce3c5ff4ca28e20af0220762a06
Jorge Moreno <jamarrecis@gmail.com>
2023-05-29 17:50:37 -0600

    ready tu upload to github

---
+++ src/factory/conexionBase.java
@@ -0,0 +1,29 @@
+package factory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class conexionBase {
+public DataSource datasource;
+
+
+public conexionBase() {
+	ComboPooledDataSource comboPool = new ComboPooledDataSource();
+	comboPool.setJdbcUrl("jdbc:mysql://localhost:3306/hotel_alura");
+	comboPool.setUser("root");
+	comboPool.setPassword("Binban182Luz!");
+	
+	this.datasource = comboPool;
+}
+public Connection recuperarConexion() {
+	try {
+		return this.datasource.getConnection();
+	} catch (SQLException e) {
+		throw new RuntimeException(e);
+	}
+}
+}
